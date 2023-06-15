1c4c589eeec9db52866e8ef57bf725152a7b07fc
dragon <2670814766@qq.com>
2023-06-15 10:30:22 +0800

    修改配置

--- src/main/java/com/dragon/blog/mapper/SysUserMapper.java
+++ src/main/java/com/dragon/blog/mapper/SysUserMapper.java
@@ -2,11 +2,13 @@ package com.dragon.blog.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dragon.blog.pojo.SysUser;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Dragon
  * @Date 2023/6/7 17:24
  * @Description
  */
+@Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
 }
